91571bed74a934ab9c879a619130da75be6ee4f5
KoreanSpaceJam <163570106+KoreanSpaceJam@users.noreply.github.com>
2024-10-17 19:56:29 +0200

    Deteccion de Jugador
    Solo lo detecta cuando el player salta, en estatico no lo detecta.

--- Assets/Scripts/EnemySkeleton.cs
+++ Assets/Scripts/EnemySkeleton.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class EnemySkeleton : Entity
 {
+    bool isAtaquing;
     [Header("Enemy Movement")]
     [SerializeField] private float movementSpeed;
+
+    [Header("Player Detection")]
+    [SerializeField] private float playerCheckDistance
+        ;
+    [SerializeField] private LayerMask whatIsPlayer;
+
+    private RaycastHit2D isPlayerDetected;
    
     protected override void Start()
     {
@@ -14,10 +22,47 @@ public class EnemySkeleton : Entity
     }
     protected override void Update()
     {
-        base.Update();
-        if (!isGrounded)
+            
+        Debug.Log(isPlayerDetected.distance);
+        if (isPlayerDetected)
+        {
+            if (isPlayerDetected.distance > 2)
+            {
+                rb.velocity = new Vector2(movementSpeed *6f * facingDirection, rb.velocity.y);
+                Debug.Log("Veo al Jugador");
+                isAtaquing = false;
+                
+            }
+            else
+            {
+                Debug.Log("Attack" + isPlayerDetected);
+                isAtaquing=true;
+            }
+        }
+        if (!isGrounded || isWallDetected )
             Flip();
 
+        movement();
+        
+
+           
+    }
+    private void movement()
+    {
+        if(!isAtaquing)
             rb.velocity = new Vector2(movementSpeed * facingDirection, rb.velocity.y);
+    }
+    protected override void CollisionChecks()
+    {
+        base.CollisionChecks();
+        isPlayerDetected = Physics2D.Raycast(transform.position, Vector2.right, playerCheckDistance * facingDirection, whatIsPlayer);
+    }
+    
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + playerCheckDistance * facingDirection, wallCheck.position.y));
     }
 }
--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -27,6 +27,11 @@ public class Entity : MonoBehaviour
         rb= GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
         
+
+        if (wallCheck== null)
+        {
+            wallCheck = transform;
+        }
     }
 
     // Update is called once per frame
@@ -37,6 +42,7 @@ public class Entity : MonoBehaviour
     protected virtual void CollisionChecks()
     {
         isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckDistance, whatIsGround);
+        isWallDetected= Physics2D.Raycast(wallCheck.position, Vector2.right, wallCheckDistance *facingDirection, whatIsGround);
     }
     protected virtual void Flip()
     {
@@ -47,7 +53,7 @@ public class Entity : MonoBehaviour
     protected virtual void OnDrawGizmos()
     {
         Gizmos.DrawLine(groundCheck.position, new Vector3(groundCheck.position.x, groundCheck.position.y - groundCheckDistance));
-        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance, wallCheck.position.y));
+        Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + wallCheckDistance * facingDirection, wallCheck.position.y));
     }
 
 
